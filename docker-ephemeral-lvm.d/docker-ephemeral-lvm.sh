943f5e0639e5c7192a5a40dd4518d2cb769a580d
shitong01 <stchin@ntu.edu.sg>
2018-12-18 18:11:19 +0800

    hack for on-premise docker volumes

--- docker-ephemeral-lvm.d/docker-ephemeral-lvm.sh
+++ docker-ephemeral-lvm.d/docker-ephemeral-lvm.sh
@@ -31,111 +31,113 @@ fi
 $stop_docker
 
 # get ephemeral storage devices
-EPH_BLK_DEVS=( `curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/ | grep ^ephemeral | sort` )
-EPH_BLK_DEVS_CNT=${#EPH_BLK_DEVS[@]}
-echo "Number of ephemeral storage devices: $EPH_BLK_DEVS_CNT"
+# EPH_BLK_DEVS=( `curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/ | grep ^ephemeral | sort` )
+# EPH_BLK_DEVS_CNT=${#EPH_BLK_DEVS[@]}
+# echo "Number of ephemeral storage devices: $EPH_BLK_DEVS_CNT"
 
-# get EBS block devices
-EBS_BLK_DEVS=( `curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/ | grep ^ebs | sort` )
-EBS_BLK_DEVS_CNT=${#EBS_BLK_DEVS[@]}
-echo "Number of EBS block devices: $EBS_BLK_DEVS_CNT"
+# # get EBS block devices
+# EBS_BLK_DEVS=( `curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/ | grep ^ebs | sort` )
+# EBS_BLK_DEVS_CNT=${#EBS_BLK_DEVS[@]}
+# echo "Number of EBS block devices: $EBS_BLK_DEVS_CNT"
 
-# set devices
-if [ "$EPH_BLK_DEVS_CNT" -ge 2 ]; then
-  DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[0]})
-  DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[1]})
-elif [ "$EPH_BLK_DEVS_CNT" -eq 1 ]; then
-  DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[0]})
-  if [ "$EBS_BLK_DEVS_CNT" -ge 1 ]; then
-    DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
-  else
-    DEV2=/dev/xvdc
-  fi
-else
-  if [ "$EBS_BLK_DEVS_CNT" -ge 2 ]; then
-    DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
-    DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[1]})
-  elif [ "$EBS_BLK_DEVS_CNT" -eq 1 ]; then
-    DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
-    DEV2=/dev/xvdc
-  else
-    DEV1=/dev/xvdb
-    DEV2=/dev/xvdc
-  fi
-fi
+# # set devices
+# if [ "$EPH_BLK_DEVS_CNT" -ge 2 ]; then
+#   DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[0]})
+#   DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[1]})
+# elif [ "$EPH_BLK_DEVS_CNT" -eq 1 ]; then
+#   DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[0]})
+#   if [ "$EBS_BLK_DEVS_CNT" -ge 1 ]; then
+#     DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
+#   else
+#     DEV2=/dev/xvdc
+#   fi
+# else
+#   if [ "$EBS_BLK_DEVS_CNT" -ge 2 ]; then
+#     DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
+#     DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[1]})
+#   elif [ "$EBS_BLK_DEVS_CNT" -eq 1 ]; then
+#     DEV1=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
+#     DEV2=/dev/xvdc
+#   else
+#     DEV1=/dev/xvdb
+#     DEV2=/dev/xvdc
+#   fi
+# fi
 
+DEV1=${1}
+DEV2=${2}
 # resolve symlinks
 DEV1=$(readlink -f $DEV1)
 DEV2=$(readlink -f $DEV2)
 
 # resolve NVMe devices
-if [[ ! -e "$DEV1" && ! -e "$DEV2" ]]; then
-  yum install -y nvme-cli || true
-  NVME_NODES=( `nvme list | grep '^/dev/' | awk '{print $1}' | sort` )
-  NVME_NODES_CNT=${#NVME_NODES[@]}
-  if [[ "${NVME_NODES_CNT}" -gt 0 ]]; then
-    # get root device and node
-    ROOT_DEV=$(df -hv / | grep '^/dev' | awk '{print $1}')
-    for nvme_dev in `nvme list | grep -v ${ROOT_DEV} | grep '^/dev/' | awk '{print $1}' | sort`; do
-      if [[ $ROOT_DEV = ${nvme_dev}* ]]; then
-        ROOT_NODE=$nvme_dev
-      fi
-    done
+# if [[ ! -e "$DEV1" && ! -e "$DEV2" ]]; then
+#   yum install -y nvme-cli || true
+#   NVME_NODES=( `nvme list | grep '^/dev/' | awk '{print $1}' | sort` )
+#   NVME_NODES_CNT=${#NVME_NODES[@]}
+#   if [[ "${NVME_NODES_CNT}" -gt 0 ]]; then
+#     # get root device and node
+#     ROOT_DEV=$(df -hv / | grep '^/dev' | awk '{print $1}')
+#     for nvme_dev in `nvme list | grep -v ${ROOT_DEV} | grep '^/dev/' | awk '{print $1}' | sort`; do
+#       if [[ $ROOT_DEV = ${nvme_dev}* ]]; then
+#         ROOT_NODE=$nvme_dev
+#       fi
+#     done
 
-    # get instance storage devices
-    if [ -z ${ROOT_NODE+x} ]; then
-        NVME_EPH_BLK_DEVS=( `nvme list |  grep '^/dev/' | grep -i 'Instance Storage' | awk '{print $1}' | sort` )
-    else
-        NVME_EPH_BLK_DEVS=( `nvme list | grep -v ${ROOT_NODE} | grep '^/dev/' | grep -i 'Instance Storage' | awk '{print $1}' | sort` )
-    fi
-    NVME_EPH_BLK_DEVS_CNT=${#NVME_EPH_BLK_DEVS[@]}
-    echo "Number of NVMe local storage block devices: $NVME_EPH_BLK_DEVS_CNT"
+#     # get instance storage devices
+#     if [ -z ${ROOT_NODE+x} ]; then
+#         NVME_EPH_BLK_DEVS=( `nvme list |  grep '^/dev/' | grep -i 'Instance Storage' | awk '{print $1}' | sort` )
+#     else
+#         NVME_EPH_BLK_DEVS=( `nvme list | grep -v ${ROOT_NODE} | grep '^/dev/' | grep -i 'Instance Storage' | awk '{print $1}' | sort` )
+#     fi
+#     NVME_EPH_BLK_DEVS_CNT=${#NVME_EPH_BLK_DEVS[@]}
+#     echo "Number of NVMe local storage block devices: $NVME_EPH_BLK_DEVS_CNT"
 
-    # get EBS devices
-    if [ -z ${ROOT_NODE+x} ]; then
-        NVME_EBS_BLK_DEVS=( `nvme list |  grep '^/dev/' | grep 'Elastic Block Store' | awk '{print $1}' | sort` )
-    else
-        NVME_EBS_BLK_DEVS=( `nvme list | grep -v ${ROOT_NODE} | grep '^/dev/' | grep 'Elastic Block Store' | awk '{print $1}' | sort` )
-    fi
-    NVME_EBS_BLK_DEVS_CNT=${#NVME_EBS_BLK_DEVS[@]}
-    echo "Number of NVMe EBS block devices: $NVME_EBS_BLK_DEVS_CNT"
+#     # get EBS devices
+#     if [ -z ${ROOT_NODE+x} ]; then
+#         NVME_EBS_BLK_DEVS=( `nvme list |  grep '^/dev/' | grep 'Elastic Block Store' | awk '{print $1}' | sort` )
+#     else
+#         NVME_EBS_BLK_DEVS=( `nvme list | grep -v ${ROOT_NODE} | grep '^/dev/' | grep 'Elastic Block Store' | awk '{print $1}' | sort` )
+#     fi
+#     NVME_EBS_BLK_DEVS_CNT=${#NVME_EBS_BLK_DEVS[@]}
+#     echo "Number of NVMe EBS block devices: $NVME_EBS_BLK_DEVS_CNT"
   
-    # assign devices
-    if [ "$NVME_EPH_BLK_DEVS_CNT" -ge 2 ]; then
-      DEV1=${NVME_EPH_BLK_DEVS[0]}
-      DEV2=${NVME_EPH_BLK_DEVS[1]}
-    elif [ "$NVME_EPH_BLK_DEVS_CNT" -eq 1 ]; then
-      DEV1=${NVME_EPH_BLK_DEVS[0]}
-      if [ "$NVME_EBS_BLK_DEVS_CNT" -ge 1 ]; then
-        DEV2=${NVME_EBS_BLK_DEVS[0]}
-      else
-        if [ "$EBS_BLK_DEVS_CNT" -ge 1 ]; then
-          DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]} | sed 's/^sd/xvd/')
-        else
-          DEV2=/dev/xvdb
-        fi
-      fi
-    else
-      if [ "$NVME_EBS_BLK_DEVS_CNT" -ge 2 ]; then
-        DEV1=${NVME_EBS_BLK_DEVS[0]}
-        DEV2=${NVME_EBS_BLK_DEVS[1]}
-      elif [ "$NVME_EBS_BLK_DEVS_CNT" -eq 1 ]; then
-        DEV1=${NVME_EBS_BLK_DEVS[0]}
-        if [ "$EBS_BLK_DEVS_CNT" -ge 1 ]; then
-          DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]} | sed 's/^sd/xvd/')
-        else
-          DEV2=/dev/xvdb
-        fi
-      else
-        DEV1=/dev/xvdb
-        DEV2=/dev/xvdc
-      fi
-    fi
-  else
-    DEV1=/dev/xvdb
-    DEV2=/dev/xvdc
-  fi
-fi
+#     # assign devices
+#     if [ "$NVME_EPH_BLK_DEVS_CNT" -ge 2 ]; then
+#       DEV1=${NVME_EPH_BLK_DEVS[0]}
+#       DEV2=${NVME_EPH_BLK_DEVS[1]}
+#     elif [ "$NVME_EPH_BLK_DEVS_CNT" -eq 1 ]; then
+#       DEV1=${NVME_EPH_BLK_DEVS[0]}
+#       if [ "$NVME_EBS_BLK_DEVS_CNT" -ge 1 ]; then
+#         DEV2=${NVME_EBS_BLK_DEVS[0]}
+#       else
+#         if [ "$EBS_BLK_DEVS_CNT" -ge 1 ]; then
+#           DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]} | sed 's/^sd/xvd/')
+#         else
+#           DEV2=/dev/xvdb
+#         fi
+#       fi
+#     else
+#       if [ "$NVME_EBS_BLK_DEVS_CNT" -ge 2 ]; then
+#         DEV1=${NVME_EBS_BLK_DEVS[0]}
+#         DEV2=${NVME_EBS_BLK_DEVS[1]}
+#       elif [ "$NVME_EBS_BLK_DEVS_CNT" -eq 1 ]; then
+#         DEV1=${NVME_EBS_BLK_DEVS[0]}
+#         if [ "$EBS_BLK_DEVS_CNT" -ge 1 ]; then
+#           DEV2=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]} | sed 's/^sd/xvd/')
+#         else
+#           DEV2=/dev/xvdb
+#         fi
+#       else
+#         DEV1=/dev/xvdb
+#         DEV2=/dev/xvdc
+#       fi
+#     fi
+#   else
+#     DEV1=/dev/xvdb
+#     DEV2=/dev/xvdc
+#   fi
+# fi
 # get sizes
 DEV1_SIZE=$(blockdev --getsize64 $DEV1)
 DEV2_SIZE=$(blockdev --getsize64 $DEV2)
@@ -147,18 +149,18 @@ echo "DEV2: $DEV2 $DEV2_SIZE"
 # delegate devices for HySDS work dir and docker storage volumes; 
 # if only one ephemeral disk, use for docker; # otherwise larger 
 # one is for HySDS work dir
-if [[ "$EPH_BLK_DEVS_CNT" -eq 1 || "$NVME_EPH_BLK_DEVS_CNT" -eq 1 ]]; then
-  DOCKER_DEV=$DEV1
-  DATA_DEV=$DEV2
-else
-  if [ "$DEV1_SIZE" -gt "$DEV2_SIZE" ]; then
+# if [[ "$EPH_BLK_DEVS_CNT" -eq 1 || "$NVME_EPH_BLK_DEVS_CNT" -eq 1 ]]; then
+#   DOCKER_DEV=$DEV1
+#   DATA_DEV=$DEV2
+# else
+if [ "$DEV1_SIZE" -gt "$DEV2_SIZE" ]; then
     DATA_DEV=$DEV1
     DOCKER_DEV=$DEV2
-  else
+else
     DATA_DEV=$DEV2
     DOCKER_DEV=$DEV1
-  fi
 fi
+# fi
 
 # log devices
 echo "DATA_DEV: $DATA_DEV"
